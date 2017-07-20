c97618e02bf05f3f82a4585b47ec340499f2de21
djskim27 <djs.kim27@gmail.com>
2017-07-20 19:57:14 -0400

    finished this goddamn hw

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,48 @@
 //your code below
+const express = require('express');
+const router = express.Router();
+const pirates = require('../models/pirates.js')
+
+//index
+router.get('/', (req,res) => {
+    res.render('../views/index.hbs', {
+    
+        pirates: pirates
+
+    })
+})
+//new
+router.get('/new', (req,res) => {
+    res.render('../views/new.hbs')
+    
+});
+//show
+router.get('/:id', (req,res) => {
+    const id = req.params.id
+    const pirate = pirates[id]
+      res.render('../views/show.hbs', {
+          pirates: pirate
+        // name: pirates[id].name,
+        // birthplace: pirates[id].birthplace,
+        // death_year: pirates[id].death_year,
+        // base: pirates[id].base,
+        // nickname: pirates[id].nickname
+    })
+})
+
+//create route
+router.post('/', (req,res) => {
+    const newPirate = {
+        name: req.body.name,
+        birthplace: req.body.birthplace,
+        death_year: req.body.death_year,
+        base: req.body.base,
+        nickname: req.body.nickname
+    }
+    console.log(req.body) 
+    pirates.push(newPirate);
+
+   res.redirect("/pirates");
+})
+
+module.exports = router;
