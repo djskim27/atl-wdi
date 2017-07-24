b049aebd9d9e2246b0196d12346de6b23a97a7dc
djskim27 <djs.kim27@gmail.com>
2017-07-24 17:25:42 -0400

    created richard and hunter

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,25 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
+class Tamagotchi {
+    constructor(name) {
+        this.propertyName = value;
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
 
+    }
+    cry(){
+        this.foodInTummy -= 1;
+        console.log(this.foodInTummy);
+        console.log("WAH");
+        return this.foodInTummy;
+    }
+};
 
 //create new Tamagotchis
-
-
+const richard = new Tamagotchi("Richard");
+const hunter = new Tamagotchi("Hunter");
+richard.cry();
+hunter.cry();
 //test out your Tamagotchies below via console.logs
--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -4,19 +4,48 @@ const fs = require('fs');
 
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
+//   let lines = content.split('\n');
 
-  // clean up the array by removing the empty line
-  lines.pop();
+//   // clean up the array by removing the empty line
+//   lines.pop();
 
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
+
+let promiseToReadFile = new Promise((resolve, reject) => {
+  fs.readFile(inFile, { encoding: 'utf8' }, (error, content) => {
+    resolve(content);
+    reject(error);
   });
 });
+  promiseToReadFile.then((result) => {
+      return result;
+    })
+    .then(()=>{
+      let lines = content.split('\n');
+      return lines;
+    })
+    .then((lines)=>{
+      lines.pop();
+      return lines;
+    })
+    .then((lines)=>{
+        lines.forEach(function(line) {
+      console.log('Hello, ' + line + '!');
+      });
+    })
+    
+      
+   
+    .catch((error)=> {
+      console.error(error);
+    })
