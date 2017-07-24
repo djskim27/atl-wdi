0f73ccc7a59d1e6c7413b8cf1c1f1f63d0d03d04
djskim27 <djs.kim27@gmail.com>
2017-07-24 01:13:19 -0400

    could not figure out update pirate function but tried my best

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -16,6 +16,8 @@ router.get('/new', (req,res) => {
     res.render('../views/new.hbs')
     
 });
+
+
 //show
 router.get('/:id', (req,res) => {
     const id = req.params.id
@@ -29,6 +31,33 @@ router.get('/:id', (req,res) => {
         // nickname: pirates[id].nickname
     })
 })
+
+//edit
+router.get('/:id/edit', (req,res) => {
+    const id = req.params.id
+    const pirate = pirates[id]
+      res.render('../views/edit.hbs', {
+          pirates: pirate
+        // name: pirates[id].name,
+        // birthplace: pirates[id].birthplace,
+        // death_year: pirates[id].death_year,
+        // base: pirates[id].base,
+        // nickname: pirates[id].nickname
+    })
+})
+
+//update
+router.put('/:id', (req,res) => {
+    const updatedPirate = pirates[req.params.id];
+
+    updatedPirate.name = req.body.name;
+    updatedPirate.birthplace = req.body.birthplace;
+    updatedPirate.death_year = req.body.death_year;
+    updatedPirate.base = req.body.base;
+    updatedPirate.nickname = req.body.nickname;
+    res.method ="GET";
+    res.redirect(`/pirates/${id}`);
+});
 
 //create route
 router.post('/', (req,res) => {
