7fd604ec4da50ad554725a6b4ad235eb2138228f
djskim27 <djs.kim27@gmail.com>
2017-07-25 09:39:08 -0400

    finished 3/7 exercises

---
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -0,0 +1,37 @@
+require('es6-promise');
+// //warm up
+// setTimeout(() => {
+//     console.log("TIMED OUT!");
+// }, 300);
+
+//  'use strict';
+
+//FULFILL A PROMISE
+
+//     var promise = new Promise(function (fulfill, reject) {
+//       // Your solution here
+//       setTimeout(() => {
+//           fulfill();
+//       },300);
+//     });
+
+//     promise.then(() => {
+//         console.log('FULFILLED!');
+//     })
+
+//REJECT A PROMISE
+var promise = new Promise(function (fulfill, reject) {
+    // Your solution here
+    setTimeout(() => {
+        reject(new Error('REJECTED!'))
+    }, 300);
+});
+
+function onReject(error) {
+    // Your solution here
+    console.log(Error.message);
+}
+
+console.log(Error.message)
+// Your solution here
+promise.then(null, onReject);
